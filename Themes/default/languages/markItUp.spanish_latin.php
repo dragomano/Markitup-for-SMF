9eea1d538941739f8392c3abf16d875263954ff0
Bomber Code <contacto@bombercode.net>
2018-11-24 18:39:19 -0300

    Translation to Latin Spanish

---
+++ Themes/default/languages/markItUp.spanish_latin.php
@@ -0,0 +1,31 @@
+<?php
+/* Spanish translation (https://www.bombercode.net) Copyright 2014-2018 */
+$txt['markItUp_settings'] = 'Editor alternativo de BBCode [markItUp!]';
+$txt['markItUp_skin'] = 'Editor de estilo';
+$txt['markItUp_disable_smileys'] = 'No mostrar emoticones';
+$txt['markItUp_areas'] = 'Acciones en las que se debe cargar un editor markItUp <div class="smalltext">Ejemplo: mp, temas</div>';
+$txt['image_desc'] = 'Texto alternativo (descripci&oacute;n)';
+$txt['image_url'] = 'URL de la imagen';
+$txt['hyperlink_text'] = 'Texto de hiperv&iacute;nculo';
+$txt['yellow_green'] = 'Amarillo-verde';
+$txt['smileys'] = 'Emoticones';
+$txt['list_item'] = 'Elemento de lista';
+$txt['table_generator'] = 'Generador de tablas';
+$txt['table_text'] = 'Su texto aqui...';
+$txt['table_question_cols'] = '&iquest;Cu&aacute;ntas columnas?';
+$txt['table_question_rows'] = '&iquest;Cu&aacute;ntas filas?';
+$txt['calculator'] = 'Calculadora';
+$txt['calculator_error'] = 'La selecci&oacute;n es imposible de calcular:';
+$txt['latex'] = 'F&oacute;rmula';
+$txt['current_day'] = 'Fecha del dia';
+$txt['alphabetic_sort'] = 'Ordenar las l&iacute;neas seleccionadas por orden alfab&eacute;tico';
+$txt['text_replace'] = 'Reemplazar el texto';
+$txt['text_replace_what'] = '&iquest;Lo que est&aacute;n buscando?';
+$txt['text_replace_with'] = '&iquest;Para reemplazar con?';
+$txt['text_replace_done'] = '&iexcl;Hecho!';
+$txt['clean'] = 'Eliminar las etiquetas BBC del texto seleccionado';
+$txt['quick_reply_desc_add'] = ' Algunos botones tienen modo de inserci&oacute;n alternativo. Intenta usar <em>Alt</em> key + Haga clic en un bot&oacute;n.';
+$txt['groups_use_markItUp'] = 'Grupos que pueden usar markItUp';
+$txt['permissionname_use_markItUp'] = 'Usar markItUp';
+$txt['permissionhelp_use_markItUp'] = 'Permitir que este grupo use markItUp.';
+<?
