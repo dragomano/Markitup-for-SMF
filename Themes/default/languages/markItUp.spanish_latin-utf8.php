73a7d295c29b9206bd54c18a946c9f4a3d48be72
Bomber Code <contacto@bombercode.net>
2018-11-24 18:39:39 -0300

    Translation to Latin Spanish

---
+++ Themes/default/languages/markItUp.spanish_latin-utf8.php
@@ -0,0 +1,31 @@
+<?php
+/* Spanish translation (https://www.bombercode.net) Copyright 2014-2018 */
+$txt['markItUp_settings'] = 'Editor alternativo de BBCode [markItUp!]';
+$txt['markItUp_skin'] = 'Editor de estilo';
+$txt['markItUp_disable_smileys'] = 'No mostrar emoticones';
+$txt['markItUp_areas'] = 'Acciones en las que se debe cargar un editor markItUp <div class="smalltext">Ejemplo: mp, temas</div>';
+$txt['image_desc'] = 'Texto alternativo (descripción)';
+$txt['image_url'] = 'URL de la imagen';
+$txt['hyperlink_text'] = 'Texto de hipervínculo';
+$txt['yellow_green'] = 'Amarillo-verde';
+$txt['smileys'] = 'Emoticones';
+$txt['list_item'] = 'Elemento de lista';
+$txt['table_generator'] = 'Generador de tablas';
+$txt['table_text'] = 'Su texto aqui...';
+$txt['table_question_cols'] = '¿Cuántas columnas?';
+$txt['table_question_rows'] = '¿Cuántas filas?';
+$txt['calculator'] = 'Calculadora';
+$txt['calculator_error'] = 'La selección es imposible de calcular:';
+$txt['latex'] = 'Fórmula';
+$txt['current_day'] = 'Fecha del dia';
+$txt['alphabetic_sort'] = 'Ordenar las líneas seleccionadas por orden alfabético';
+$txt['text_replace'] = 'Reemplazar el texto';
+$txt['text_replace_what'] = '¿Lo que están buscando?';
+$txt['text_replace_with'] = '¿Para reemplazar con?';
+$txt['text_replace_done'] = '¡Hecho!';
+$txt['clean'] = 'Eliminar las etiquetas BBC del texto seleccionado';
+$txt['quick_reply_desc_add'] = ' Algunos botones tienen modo de inserción alternativo. Intenta usar <em>Alt</em> key + Haga clic en un botón.';
+$txt['groups_use_markItUp'] = 'Grupos que pueden usar markItUp';
+$txt['permissionname_use_markItUp'] = 'Usar markItUp';
+$txt['permissionhelp_use_markItUp'] = 'Permitir que este grupo use markItUp.';
+<?
